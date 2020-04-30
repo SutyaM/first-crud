652cb314cb228fc255d896ea390393ae91e1fd24
SutyaM <sutya.miklos.cv@gmail.com>
2020-04-30 16:49:34 +0200

    swagger created

--- src/app/routers/group.ts
+++ src/app/routers/group.ts
@@ -3,9 +3,9 @@ import * as groupController from '../controllers/group';
 
 export const router: Router = Router({ mergeParams: true });
 
-router.use(groupController.authorization);
 router.get('/', groupController.index);
 router.get('/:id', groupController.show);
+router.use(groupController.authorization);
 router.post('/', groupController.create);
 router.put('/:id', groupController.update);
 router.delete('/:id', groupController.destroy);
